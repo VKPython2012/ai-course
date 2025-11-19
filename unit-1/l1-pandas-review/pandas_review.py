fd0ae598c492e6a040bba8a94af791a0a5d2b469
VKPython2012 <vinhkhoi1212@gmail.com>
2025-11-19 19:32:21 +0700

    modified:   unit-1/l1-pandas-review/pandas_review.py

--- unit-1/l1-pandas-review/pandas_review.py
+++ unit-1/l1-pandas-review/pandas_review.py
@@ -28,3 +28,55 @@ df_cleaned["Exercise_Frequency"].fillna(0, inplace=True)
 
 # Filtering Data
 # Filter rows where BMI > 25 & Sleep Hours > 7
+filtered_df = df_cleaned[(df_cleaned["BMI"] > 25) & (df_cleaned["Sleep_Hours"] > 7)]
+print(f"\nFitlered DF (BMI > 25 % Sleep Hours > 7):")
+print(filtered_df)
+
+### Grouping & Aggregrating Data
+# Group Data (by smoking status and calculate summary statistics)
+grouped_df = filtered_df.groupby("Smoking_Status").agg(
+    Avg_Health_Score = ("Health_Score", "mean"),
+    Avg_Exercise = ("Exercise_Frequency", "mean"),
+    Total_Alcohol_Consumption = ("Alcohol_Consumption", "sum")
+).reset_index()
+print(f"\nGrouped Data by Smoking Status")
+print(grouped_df)
+
+### Questions ###
+
+questions = [
+    "1. What is the average health scorer for smokers and non-smokers?",
+    "2. How does BMI correlate with health score for individuals who exercise frequently?",
+    "3. What is the total alcohol consumption for each smoking status group?",
+    "4. What is the average exercise frequency among individuals with BMI over 30?",
+    "5. How does the amount of sleep hours relate to health score on average?"
+]
+
+print("\nQuestions")
+for q in questions:
+    print(q)
+
+# 1. What is the average health scorer for smokers and non-smokers?
+avg_health_score = df_cleaned.groupby("Smoking_Status")["Health_Score"].mean().reset_index()
+avg_health_score.rename(columns={"Health_Score":"Avg_Health_Score"}, inplace=True)
+print('\n1. What is the average health scorer for smokers and non-smokers?')
+print(avg_health_score)
+
+# 2. How does BMI correlate with health score for individuals who exercise frequently?
+# print(df_cleaned["Exercise_Frequency"].mean())
+# print(df_cleaned["Exercise_Frequency"].quantile())
+# print(df_cleaned["Exercise_Frequency"].max())
+frequent_exercisers = df_cleaned[df_cleaned["Exercise_Frequency"] > 4]
+colleration_BMI_health = frequent_exercisers[["BMI", "Health_Score"]].corr().iloc[0, 1]
+print("\n2. How does BMI correlate with health score for individuals who exercise frequently?")
+print(colleration_BMI_health)
+
+# 3. What is the total alcohol consumption for each smoking status group?
+total_alcohol_consumption = grouped_df[["Smoking_Status", "Total_Alcohol_Consumption"]]
+print("\n3. What is the total alcohol consumption for each smoking status group?")
+print(total_alcohol_consumption)
+
+# 4. What is the exercise frequency among individuals with BMI over 30?"
+avg_exercise_bmi_30 = df_cleaned[df_cleaned["BMI"] > 30]["Exercise_Frequency"].mean()
+print("\n4. What is the exercisr frequency among individuals with BMI over 30?")
+print(avg_exercise_bmi_30)
